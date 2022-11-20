d30c6c72acf249db665548bc3f00e91a5a3e8734
omar <omar>
2022-11-20 11:38:26 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ParallaxEffectApp());
+
+class ParallaxEffectApp extends StatelessWidget {
+  const ParallaxEffectApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: SafeArea(child: ,)
+      ),
+    );
+  }
+}
